1a94a8e52877a6de19ecb51d272fa3b2cb1e98f5
jprasham <119007126+jprasham@users.noreply.github.com>
2024-01-02 16:38:33 +0530

    Update ETF_MODEL_STREAMLIT.py

--- ETF_MODEL_STREAMLIT.py
+++ ETF_MODEL_STREAMLIT.py
@@ -209,11 +209,11 @@ st.write('## Excess Returns')
 st.write('### Excess Return above 80 percentile')
 st.dataframe(etf_ex_1[['Ticker','Name','Category','Sub category','Price','PctRank_1M','PctRank_3M','PctRank_6M','PctRank_12M','HistExcessReturn_1M','HistExcessReturn_3M','HistExcessReturn_6M','HistExcessReturn_12M','ChgRnk_1M','ChgRnk_3M','ChgRnk_6M','ChgRnk_12M']])
 
-st.write('### Excess Return below 20 percentile')
+st.write('### Excess Return below 20 percentile)
 st.dataframe(etf_ex_2[['Ticker','Name','Category','Sub category','Price','PctRank_1M','PctRank_3M','PctRank_6M','PctRank_12M','HistExcessReturn_1M','HistExcessReturn_3M','HistExcessReturn_6M','HistExcessReturn_12M','ChgRnk_1M','ChgRnk_3M','ChgRnk_6M','ChgRnk_12M']])
 
 st.write('## High volatility and Large Drawdowns')
 st.dataframe(etf_vol[['Ticker','Name','Category','Sub category','Price','PctRank_1M','PctRank_3M','PctRank_6M','PctRank_12M','HistExcessReturn_1M','HistExcessReturn_3M','HistExcessReturn_6M','HistExcessReturn_12M','ChgRnk_1M','ChgRnk_3M','ChgRnk_6M','ChgRnk_12M']])
 
-st.write('## Excess Returms > 20 and losing Short Term Momentum(50DMA)')
+st.write('## Excess Returns > 20 and losing Short Term Momentum(50DMA)')
 st.dataframe(etf_ex50[['Ticker','Name','Category','Sub category','Price','PctRank_1M','PctRank_3M','PctRank_6M','PctRank_12M','HistExcessReturn_1M','HistExcessReturn_3M','HistExcessReturn_6M','HistExcessReturn_12M','ChgRnk_1M','ChgRnk_3M','ChgRnk_6M','ChgRnk_12M']])
